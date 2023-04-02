10318ad3585823b6dd10ea6378843e1952f51403
mstrmindz <47842133+mstrmindz@users.noreply.github.com>
2023-04-02 11:29:01 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,15 @@
 #!/usr/bin/node
-function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-console.log(factorial(Number(process.argv[2])));
+const arg = parseInt(process.argv[2]);
+const result = factorial(arg);
+
+console.log(`Factorial of ${arg} is ${result}`);
